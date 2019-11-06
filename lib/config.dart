f3cfb7e2c351fbfd5e97ffab8023272e189ee6a9
Levente Novotny <Novotny.Levente@gmail.com>
2019-11-06 13:34:41 +0100

    Create config.dart

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+//These are not real values
+//Please do not use these keys in production
+
+String passKey = "PASSWORD";
+String codeKey = "PASSWORD";
+String userKey = "PASSWORD";
+String iv = 'IV';
