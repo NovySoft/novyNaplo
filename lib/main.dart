438d4e1eb393cc93282d25e2769c886ce64e7afe
Levente Novotny <legolaszstudio@gmail.com>
2019-12-06 19:59:59 +0100

    Added analytics and crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:novynaplo/functions/colorManager.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 //import 'package:permission_handler/permission_handler.dart';
 import 'package:novynaplo/screens/avarages_tab.dart';
 import 'package:novynaplo/screens/marks_tab.dart';
 import 'package:novynaplo/screens/settings_tab.dart';
 import 'package:novynaplo/screens/login_page.dart';
+FirebaseAnalytics analytics = FirebaseAnalytics();
 
 
 void main() async {
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
   runApp(MyApp());
   /*Map<PermissionGroup, PermissionStatus> permissions =
       await PermissionHandler().requestPermissions([PermissionGroup.contacts]);*/
@@ -34,6 +40,9 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             home: LoginPage(),
             routes: routes,
+            navigatorObservers: [
+              FirebaseAnalyticsObserver(analytics: analytics),
+            ],
           );
         });
   }
