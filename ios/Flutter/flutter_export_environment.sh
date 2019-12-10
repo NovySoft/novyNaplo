13850141ab3f324d687c3c8badcb5dd1bda00ae6
Novy <novotny.levente@gmail.com>
2019-12-10 20:31:34 +0100

    Modified .gitignore Countinued working on notices (checkpoint)

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\FLUTTER\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\novynotifier"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/novy/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/novy/novynotifier"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=E:\FLUTTER\flutter\bin\cache\artifacts\engine\ios"
-export "FLUTTER_BUILD_NAME=0.0.3"
-export "FLUTTER_BUILD_NUMBER=0.0.3"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/novy/flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.0.8"
+export "FLUTTER_BUILD_NUMBER=0.0.8"
