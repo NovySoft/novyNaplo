78984343aa246a61ebbffa56535f10c2fe9e5474
Levente Novotny <Novotny.Levente@gmail.com>
2020-03-25 19:21:19 +0100

    PieChart

--- lib/helpers/chartHelper.dart
+++ lib/helpers/chartHelper.dart
@@ -1,6 +1,8 @@
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:novynaplo/screens/statistics_tab.dart' as stats;
 import 'dart:math';
 
+//TODO optimize this entire thing
 class LinearMarkChartData {
   final int count;
   final double value;
@@ -64,13 +66,43 @@ List<LinearMarkChartData> makeChartPoints(var list) {
 List<charts.Series<LinearMarkChartData, int>> makeChartReturnList(input) {
   List<charts.Series<LinearMarkChartData, int>> returnList = [];
   List<LinearMarkChartData> tempList = [];
+  var chartTempList = [
+    charts.MaterialPalette.green.shadeDefault,
+    charts.MaterialPalette.blue.shadeDefault,
+    charts.MaterialPalette.red.shadeDefault,
+    charts.MaterialPalette.deepOrange.shadeDefault,
+    charts.MaterialPalette.lime.shadeDefault,
+    charts.MaterialPalette.purple.shadeDefault,
+    charts.MaterialPalette.cyan.shadeDefault,
+    charts.MaterialPalette.yellow.shadeDefault,
+    charts.MaterialPalette.indigo.shadeDefault,
+    charts.MaterialPalette.pink.shadeDefault,
+    charts.MaterialPalette.teal.shadeDefault,
+  ];
   int index = 0;
   for (var n in input) {
     tempList = [];
     for (var y in n) {
       tempList.add(y);
     }
-    var color = chartColorList[getRandomBetween(0, chartColorList.length-1)];
+    if (chartTempList.length == 0) {
+      chartTempList = [
+        charts.MaterialPalette.green.shadeDefault,
+        charts.MaterialPalette.blue.shadeDefault,
+        charts.MaterialPalette.red.shadeDefault,
+        charts.MaterialPalette.deepOrange.shadeDefault,
+        charts.MaterialPalette.lime.shadeDefault,
+        charts.MaterialPalette.purple.shadeDefault,
+        charts.MaterialPalette.cyan.shadeDefault,
+        charts.MaterialPalette.yellow.shadeDefault,
+        charts.MaterialPalette.indigo.shadeDefault,
+        charts.MaterialPalette.pink.shadeDefault,
+        charts.MaterialPalette.teal.shadeDefault,
+      ];
+    }
+    var rndInt = getRandomBetween(0, chartTempList.length);
+    var color = chartTempList[rndInt];
+    chartTempList.removeAt(rndInt);
     returnList.add(new charts.Series<LinearMarkChartData, int>(
         id: tempList[0].id,
         colorFn: (_, __) => color,
@@ -82,7 +114,91 @@ List<charts.Series<LinearMarkChartData, int>> makeChartReturnList(input) {
   return returnList;
 }
 
-int getRandomBetween(int min, int max){
+int getRandomBetween(int min, int max) {
   final _random = new Random();
   return min + _random.nextInt(max - min);
+}
+
+void getAllSubjectsAv(input) {
+  int index = 1;
+  int sum = 0;
+  for (var n in input[0]) {
+    sum += int.parse(n.split(":")[1]);
+    stats.globalAllSubjectAv.value = sum / index;
+    if (index == input[0].length - 1) {
+      stats.globalAllSubjectAv.diffSinceLast = sum / index;
+    }
+    index++;
+  }
+  stats.globalAllSubjectAv.diffSinceLast =
+      (stats.globalAllSubjectAv.diffSinceLast - sum / (index - 1)) * -1;
+}
+
+void getWorstAndBest(input) {
+  //TODO extend to on 100% marks
+  List<stats.AV> tempList = [];
+  stats.AV temp = new stats.AV();
+  int index = 1;
+  int sum = 0;
+  for (var n in input) {
+    index = 1;
+    sum = 0;
+    for (var y in n) {
+      sum += int.parse(y.split(":")[1]);
+      temp.subject = y.split(":")[0];
+      temp.value = sum / index;
+      if (index == n.length - 1) {
+        temp.diffSinceLast = sum / index;
+      }
+      index++;
+    }
+    temp.count = index;
+    temp.diffSinceLast = (temp.diffSinceLast - sum / (index - 1)) * -1;
+    tempList.add(temp);
+    temp = new stats.AV();
+  }
+  tempList.sort((a, b) =>
+      b.value.toStringAsFixed(3).compareTo(a.value.toStringAsFixed(3)));
+  stats.worstSubjectAv = tempList.last;
+  double curValue = tempList[0].value;
+  index = 0;
+  List<stats.AV> tempListTwo = [];
+  while (curValue == tempList[index].value) {
+    tempListTwo.add(tempList[index]);
+    index++;
+  }
+  tempListTwo.sort((a, b) => b.count.compareTo(a.count));
+  stats.bestSubjectAv = tempListTwo[0];
+}
+
+void getPieChart(var input) {
+  List<stats.LinearPiData> tempData = [];
+  int index = 0;
+  String name = "";
+  for (var n in input) {
+    if(n[0].split(":")[0].toLowerCase().startsWith("magyar")){
+      name = n[0].split(":")[0].split(" ")[1];
+    }else{
+      name = n[0].split(":")[0];
+    }
+    if (index != 0) {
+      tempData.add(new stats.LinearPiData(index, n.length, name));
+    }
+    index++;
+  }
+  tempData.sort((a, b) => a.value.compareTo(b.value));
+  stats.pieList = [
+    new charts.Series<stats.LinearPiData, int>(
+      id: 'MarksCountPie',
+      colorFn: (_, index) {
+        return charts.MaterialPalette.blue.shadeDefault;
+      },
+      domainFn: (stats.LinearPiData sales, _) => sales.id,
+      measureFn: (stats.LinearPiData sales, _) => sales.value,
+      data: tempData,
+      // Set a label accessor to control the text of the arc label.
+      labelAccessorFn: (stats.LinearPiData row, _) =>
+          '${row.name}: ${row.value}',
+    )
+  ];
 }
--- lib/screens/statistics_tab.dart
+++ lib/screens/statistics_tab.dart
@@ -18,6 +18,26 @@ final List<Tab> statTabs = <Tab>[
 ];
 TabController _tabController;
 List<charts.Series<LinearMarkChartData, int>> allSubjectsChartData;
+AV globalAllSubjectAv = new AV();
+AV worstSubjectAv = new AV();
+AV bestSubjectAv = new AV();
+List<charts.Series> pieList;
+
+//Classes used by charts
+class AV {
+  double value;
+  double diffSinceLast;
+  String subject = "";
+  int count = 0;
+}
+
+class LinearPiData {
+  final int id;
+  final int value;
+  final String name;
+
+  LinearPiData(this.id, this.value, this.name);
+}
 
 class StatisticsTab extends StatefulWidget {
   static String tag = 'statistics';
@@ -44,6 +64,56 @@ class _StatisticsTabState extends State<StatisticsTab>
           controller: _tabController,
           children: statTabs.map((Tab tab) {
             if (tab.text.toLowerCase() == "általános") {
+              Color avColor, worstAvColor, bestAvColor;
+              Icon avIcon, worstAvIcon, bestAvIcon;
+              setState(() {
+                getAllSubjectsAv(allParsedSubjects);
+                getWorstAndBest(allParsedSubjects);
+                getPieChart(allParsedSubjects);
+                if (globalAllSubjectAv.diffSinceLast == 0) {
+                  avColor = Colors.orange;
+                  avIcon = Icon(
+                    Icons.linear_scale,
+                    color: avColor,
+                  );
+                } else if (globalAllSubjectAv.diffSinceLast < 0) {
+                  avColor = Colors.red;
+                  avIcon = Icon(Icons.keyboard_arrow_down, color: avColor);
+                } else if (globalAllSubjectAv.diffSinceLast > 0) {
+                  avColor = Colors.green;
+                  avIcon = Icon(Icons.keyboard_arrow_up, color: avColor);
+                }
+                if (worstSubjectAv.diffSinceLast == 0) {
+                  worstAvColor = Colors.orange;
+                  worstAvIcon = Icon(
+                    Icons.linear_scale,
+                    color: worstAvColor,
+                  );
+                } else if (worstSubjectAv.diffSinceLast < 0) {
+                  worstAvColor = Colors.red;
+                  worstAvIcon =
+                      Icon(Icons.keyboard_arrow_down, color: worstAvColor);
+                } else if (worstSubjectAv.diffSinceLast > 0) {
+                  worstAvColor = Colors.green;
+                  worstAvIcon =
+                      Icon(Icons.keyboard_arrow_up, color: worstAvColor);
+                }
+                if (bestSubjectAv.diffSinceLast == 0) {
+                  bestAvColor = Colors.orange;
+                  bestAvIcon = Icon(
+                    Icons.linear_scale,
+                    color: bestAvColor,
+                  );
+                } else if (bestSubjectAv.diffSinceLast < 0) {
+                  bestAvColor = Colors.red;
+                  bestAvIcon =
+                      Icon(Icons.keyboard_arrow_down, color: bestAvColor);
+                } else if (bestSubjectAv.diffSinceLast > 0) {
+                  bestAvColor = Colors.green;
+                  bestAvIcon =
+                      Icon(Icons.keyboard_arrow_up, color: bestAvColor);
+                }
+              });
               final axis = charts.NumericAxisSpec(
                   renderSpec: charts.GridlineRendererSpec(
                       labelStyle: charts.TextStyleSpec(
@@ -57,29 +127,124 @@ class _StatisticsTabState extends State<StatisticsTab>
                     fontSize: 10,
                     color: charts.MaterialPalette.blue.shadeDefault),
               ));
-              return Column(
-                children: [
-                  SizedBox(
-                    height: 500,
-                    child: charts.NumericComboChart(
-                      createAllSubjectChartData(allParsedSubjects),
-                      animate: false,
-                      domainAxis: axisTwo,
-                      primaryMeasureAxis: axis,
-                      // Configure the default renderer as a line renderer. This will be used
-                      // for any series that does not define a rendererIdKey.
-                      defaultRenderer:
-                          new charts.LineRendererConfig(includePoints: true),
-                      behaviors: [
-                        new charts.SeriesLegend(
-                            position: charts.BehaviorPosition.end),
-                        new charts.PanAndZoomBehavior()
-                      ],
-                    ),
-                  )
-                ],
+              return ListView.builder(
+                itemCount: 8,
+                padding: EdgeInsets.symmetric(vertical: 12),
+                itemBuilder: (BuildContext context, int index) {
+                  switch (index) {
+                    case 0:
+                      return SizedBox(
+                        height: 500,
+                        child: charts.NumericComboChart(
+                          createAllSubjectChartData(allParsedSubjects),
+                          animate: true,
+                          domainAxis: axisTwo,
+                          primaryMeasureAxis: axis,
+                          // Configure the default renderer as a line renderer. This will be used
+                          // for any series that does not define a rendererIdKey.
+                          defaultRenderer: new charts.LineRendererConfig(
+                              includePoints: true),
+                          behaviors: [
+                            new charts.SeriesLegend(
+                              position: charts.BehaviorPosition.end,
+                            ),
+                            new charts.PanAndZoomBehavior()
+                          ],
+                        ),
+                      );
+                      break;
+                    case 1:
+                      return SizedBox(
+                        height: 25,
+                      );
+                      break;
+                    case 2:
+                      return Row(
+                        children: <Widget>[
+                          Text(
+                            "Összesített átlag: " +
+                                globalAllSubjectAv.value.toStringAsFixed(3),
+                            textAlign: TextAlign.start,
+                            style: new TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          avIcon,
+                          Text(
+                            globalAllSubjectAv.diffSinceLast.toStringAsFixed(3),
+                            style: TextStyle(color: avColor),
+                          )
+                        ],
+                      );
+                      break;
+                    case 3:
+                      return Row(
+                        children: <Widget>[
+                          Text(
+                            "Legroszabb (" + worstSubjectAv.subject + ")",
+                            textAlign: TextAlign.start,
+                            style: new TextStyle(color: Colors.red),
+                          ),
+                          Text(
+                            " átlag: " +
+                                worstSubjectAv.value.toStringAsFixed(3),
+                            textAlign: TextAlign.start,
+                          ),
+                          worstAvIcon,
+                          Text(
+                            worstSubjectAv.diffSinceLast.toStringAsFixed(3),
+                            style: TextStyle(color: worstAvColor),
+                          )
+                        ],
+                      );
+                      break;
+                    case 4:
+                      return Row(
+                        children: <Widget>[
+                          Text(
+                            "Legjobb (" + bestSubjectAv.subject + ")",
+                            textAlign: TextAlign.start,
+                            style: new TextStyle(color: Colors.green),
+                          ),
+                          Text(
+                            " átlag: " + bestSubjectAv.value.toStringAsFixed(3),
+                            textAlign: TextAlign.start,
+                          ),
+                          bestAvIcon,
+                          Text(
+                            bestSubjectAv.diffSinceLast.toStringAsFixed(3),
+                            style: TextStyle(color: bestAvColor),
+                          ),
+                        ],
+                      );
+                      break;
+                    case 5:
+                      return SizedBox(
+                        height: 25,
+                      );
+                      break;
+                    case 6:
+                      return Center(
+                        child: Text("Jegyek száma bizonyos tantárgyakból:",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                      );
+                      break;
+                    case 7:
+                      return SizedBox(
+                          height: 500,
+                          width: double.infinity,
+                          child: new charts.PieChart(
+                            pieList,
+                            animate: true,
+                            defaultRenderer: new charts.ArcRendererConfig(
+                                arcRendererDecorators: [
+                                  new charts.ArcLabelDecorator(
+                                      labelPosition:
+                                          charts.ArcLabelPosition.inside)
+                                ]),
+                          ));
+                      break;
+                  }
+                },
               );
-              //return Text("alma");
             } else {
               return ListView.builder(
                   itemCount: allParsedSubjects.length + globals.adModifier,
