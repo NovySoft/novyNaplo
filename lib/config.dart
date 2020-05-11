f6da25dd41382f1dcf62737e9f71d5f8519aaba7
Levente Novotny <Novotny.Levente@gmail.com>
2020-05-11 19:10:03 +0200

    SQLBETA

--- lib/config.dart
+++ lib/config.dart
@@ -20,8 +20,8 @@ String bannerUnitId = "ca-app-pub-6768960612393878/1881515932";
 //Production key: "ca-app-pub-6768960612393878/1881515932"
 
 //App version, so I can do special version names:
-String currentAppVersionCode = "V0.1.4.2";
+String currentAppVersionCode = "V0.1.4.2+SQLBETA";
 //Is the app final release? (aka is/was in version.json)
-//If false don't do the new version check
+//!If false don't do the new version check
 //Mainly for travis ci builds
-bool isAppRelease = true;
+bool isAppRelease = false;
