f3df721b8880e42b086de1dfbb8cd465423d2176
Levente Novotny <Novotny.Levente@gmail.com>
2020-05-27 17:50:53 +0200

    update capitalize

--- lib/functions/utils.dart
+++ lib/functions/utils.dart
@@ -83,7 +83,12 @@ List<String> getRandomNames(int amount) {
 }
 
 String capitalize(String word) {
-  if (word == null || word == "") return "";
+  if (word == null ||
+      word == "" ||
+      word.length == 0 ||
+      (word is String) == false) {
+    return "";
+  }
   if (word.length < 2) return word;
   return '${word[0].toUpperCase()}${word.substring(1).toLowerCase()}';
 }
