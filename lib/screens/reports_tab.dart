c2d07f72e30c60d0bd951451fcef04d7c414374a
Levente Novotny <Novotny.Levente@gmail.com>
2020-08-13 14:46:13 +0200

    Added some todos
    I'm on holiday....

--- lib/screens/reports_tab.dart
+++ lib/screens/reports_tab.dart
@@ -9,7 +9,8 @@ import 'package:novynaplo/screens/reports_detail_tab.dart';
 import 'package:novynaplo/translations/translationProvider.dart';
 import 'package:novynaplo/screens/marks_tab.dart' as marks;
 import 'package:novynaplo/screens/statistics_tab.dart' as stats;
-
+//TODO: add performance to mark calculator and statistics
+//TODO: add összesített nézet to statistics
 TabController _tabController;
 final List<Tab> reportTabs = <Tab>[
   //*"Type": "IQuarterEvaluation",
