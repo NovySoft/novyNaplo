46c19444f3657f5677879a662dc5adc78c548739
Novotny Levente <Novotny.Levente@gmail.com>
2021-02-05 17:15:05 +0100

    Bump version

--- lib/config.dart
+++ lib/config.dart
@@ -21,7 +21,7 @@ String bannerUnitId = "ca-app-pub-6768960612393878/1881515932";
 //Production key: "ca-app-pub-6768960612393878/1881515932"
 
 //App version, so I can do special version names:
-String currentAppVersionCode = "V1.0.6+26 APIv3 Alpha3";
+String currentAppVersionCode = "V1.1.0+27";
 
 //Is the app final release? (aka is/was in version.json)
 //!If false don't do the new version check
