203f58ea402517b673873b03c781e09888e96303
Levente Novotny <Novotny.Levente@gmail.com>
2021-08-16 19:56:44 +0200

    Grammar error

--- lib/API/calcKretaNonce.dart
+++ lib/API/calcKretaNonce.dart
@@ -7,7 +7,7 @@ Tisztelt E-kréta!
 
 Remélem olvassák ezt a kis üzenetet, ha már a leveleimet nem…
 
-Nem tudom, hogy önök szándékosan keserítik/nehezítik meg az alternatív kliens fejlesztők életét, de a módszerek melyeket bevetnek, még egy kezdő 15 éves programozót (pl.: engem) sem képesek visszatartani a folytatástól, viszont egy levél, melyben a „NEM” szó található viszont már képes lenne. Tény, hogy vannak dolgok melyeket nem ideális módon önöktől kérve szerzünk meg, mint pl. a lentebb található encoder kulcs, api kulcsok vagy esetleg a client_id, de, hogyan szerezhetnénk meg ezeket a „titkokat” „nem sértő módon”, ha nem válaszolnak a leveleinkre?
+Nem tudom, hogy önök szándékosan keserítik/nehezítik meg az alternatív kliens fejlesztők életét, de a módszerek melyeket bevetnek, még egy kezdő 15 éves programozót (pl.: engem) sem képesek visszatartani a folytatástól, viszont egy levél, melyben a „NEM” szó található már képes lenne. Tény, hogy vannak dolgok melyeket nem ideális módon önöktől kérve szerzünk meg, mint pl. a lentebb található encoder kulcs, api kulcsok vagy esetleg a client_id, de, hogyan szerezhetnénk meg ezeket a „titkokat” „nem sértő módon”, ha nem válaszolnak a leveleinkre?
 
 A lényeg az, hogy az alábbi kulcs se ideálisan önöktől lett elkérve, amennyiben ez önöket sérti vagy szeretnének megállapodás alapján „publikus kulcsot” bevezetni, legyenek szívesek írjanak egy levelet a novysoftware@gmail.com -ra.
 
