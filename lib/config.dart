bdb70b81e9468f809a7b90f80ffb8aff4366314a
Levente Novotny <Novotny.Levente@gmail.com>
2021-08-30 21:51:10 +0200

    Bump version

--- lib/config.dart
+++ lib/config.dart
@@ -13,4 +13,4 @@ final String clientId = "kreta-ellenorzo-mobile";
 String menuLogo = "assets/home.png";
 
 //App version, so I can do special version names:
-String currentAppVersionCode = "V1.1.5+11+multiuser-devpoint3";
+String currentAppVersionCode = "V1.1.5+11+multiuser-devpoint4";
