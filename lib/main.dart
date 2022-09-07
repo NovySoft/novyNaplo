4ffe55907955141cf645dbfebda1bd8cc772d830
Legolaszstudio <novotny.levente@gmail.com>
2022-09-07 17:00:05 +0200

    Update firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:novynaplo/data/database/databaseHelper.dart';
@@ -44,7 +45,9 @@ class NavigatorKey {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await globals.setGlobals();
   if (foundation.kDebugMode) {
     print("Firebase disabled");
